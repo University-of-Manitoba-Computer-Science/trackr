a34381d69fc5830abbb49de8965374030abb41ee
awphung <awphung@gmail.com>
2023-07-09 22:03:16 -0500

    Added json parser to dependencies
    Serde dependency added

--- Packages/rust/src/trackr.rs
+++ Packages/rust/src/trackr.rs
@@ -1,41 +1,98 @@
 use reqwest::Client;
-use std::thread;
-use std::time::Duration;
+use serde::{Deserialize, Serialize};
 
 const API_ENDPOINT: &str = "http://wryneck.cs.umanitoba.ca/api/values";
 
-pub async fn add_single_value(api_key: &str, field_id: i32, value: &str) -> Result<String, reqwest::Error> {
-    let client = Client::new();
-    let data = [("apiKey", api_key), ("value", value), ("fieldId", &field_id.to_string())];
-    let response = client.post(API_ENDPOINT).form(&data).send().await?;
-    let body = response.text().await?;
-    Ok(body)
+#[derive(Debug, Serialize, Deserialize)]
+struct ApiResponse {
+    status_code: u16,
+    text: String,
+    // Add other fields as needed
 }
 
-pub async fn add_many_values(api_key: &str, field_id: i32, values: &[&str]) -> Result<String, reqwest::Error> {
-    let mut result = String::new();
+pub struct Trackr {
+    api_key: String,
+    api_url: String,
+    last_call_timestamp: time::Duration,
+}
 
-    for value in values {
-        let response = add_single_value(api_key, field_id, value).await?;
-        if response != "200" {
-            return Ok(response);
+impl Trackr {
+    pub fn new(api_key: String) -> Self {
+        Trackr {
+            api_key,
+            api_url: "http://wryneck.cs.umanitoba.ca".to_string(),
+            last_call_timestamp: time::Duration::from_secs(0),
         }
-        thread::sleep(Duration::from_secs(1));
-        result = response;
     }
-    Ok(result)
+
+    pub fn set_api_key(&mut self, api_key: String) {
+        self.api_key = api_key;
+    }
+
+    pub fn set_api_url(&mut self, api_url: String) {
+        self.api_url = api_url;
+    }
+
+    pub async fn add_single_value(&self, field_id: u32, value: i32) -> Result<ApiResponse, reqwest::Error> {
+        let client = Client::new();
+        let data = [
+            ("fieldId", field_id.to_string()),
+            ("value", value.to_string()),
+            ("apiKey", self.api_key.clone()),
+        ];
+
+        let response = client.post(API_ENDPOINT).form(&data).send().await?;
+        let api_response: ApiResponse = response.json().await?;
+
+        Ok(api_response)
+    }
+
+    pub async fn add_many_values(&self, field_id: u32, values: &[i32]) -> Result<ApiResponse, reqwest::Error> {
+        let client = Client::new();
+
+        let mut api_responses = Vec::new();
+        for value in values {
+            let api_response = self.add_single_value(field_id, *value).await?;
+            api_responses.push(api_response);
+        }
+
+        Ok(api_responses[api_responses.len() - 1].clone())
+    }
+
+    pub async fn get_values(
+        &self,
+        field_id: u32,
+        offset: u32,
+        limit: u32,
+        order: &str,
+    ) -> Result<GetValuesResponse, reqwest::Error> {
+        let client = Client::new();
+        let params = [
+            ("apiKey", &self.api_key),
+            ("fieldId", &field_id.to_string()),
+            ("offset", &offset.to_string()),
+            ("limit", &limit.to_string()),
+            ("order", order),
+        ];
+
+        let response = client.get(API_ENDPOINT).query(&params).send().await?;
+        let api_response: GetValuesResponse = response.json().await?;
+
+        Ok(api_response)
+    }
 }
 
-pub async fn get_values(api_key: &str, field_id: i32, offset: i32, limit: i32, order: &str) -> Result<String, reqwest::Error> {
-    let client = Client::new();
-    let params = [
-        ("apiKey", api_key),
-        ("fieldId", &field_id.to_string()),
-        ("offset", &offset.to_string()),
-        ("limit", &limit.to_string()),
-        ("order", order),
-    ];
-    let response = client.get(API_ENDPOINT).query(&params).send().await?;
-    let body = response.text().await?;
-    Ok(body)
+#[derive(Debug, Serialize, Deserialize)]
+struct Value {
+    id: u32,
+    value: String,
+    created_at: String,
+    // Add other fields as needed
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GetValuesResponse {
+    values: Vec<Value>,
+    total_values: u32,
+    // Add other fields as needed
 }
